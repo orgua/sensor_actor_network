655ea908ad04e6757faba1862fd1cf72596fd6fd
orgua <splingmael@gmail.com>
2015-09-21 22:54:15 +0200

    unionize data

--- src/layer_network.h
+++ src/layer_network.h
@@ -8,13 +8,19 @@
 
 #include "layer_interface.h"
 
-struct Control // bitfield
+typedef union
 {
-    bool wantsAck  : 1;
-    bool isAck     : 1;
-    bool isRequest : 1;
-    bool reserved  : 1;  // has tbd
-};
+    struct // bitfield
+    {
+        bool wantsAck  : 1;
+        bool isAck     : 1;
+        bool isRequest : 1;
+        bool reserved  : 1;  // has tbd
+    } bit;
+
+    uint8_t value;
+} Control;
+
 //constexpr uint8_t Control_size = sizeof(Control);
 //#if (sizeof(Control) != 1)
 //#error "BITFIELD is larger than 1 Byte"
@@ -22,23 +28,45 @@ struct Control // bitfield
 
 class layer_network : public layer_interface
 {
-   void handle_receive(stack_message *msg)
-   {
-       if (DEBUG)  cout << "rNetwork ";
-       // handle header and payload
-       if (!is_top) p_upper_layer->handle_receive(msg);
-       // handle tail
-   }
+public:
 
-   void handle_transmit(stack_message *msg)
-   {
-       if (DEBUG)  cout << "tNetwork ";
-       // handle header and payload
-       msg->payload[msg->position++] = 1;
-       msg->size++;
-       if (!is_top) p_upper_layer->handle_transmit(msg);
-       // handle tail
-   };
+    Control control; // is public, so readable from outside
+
+    layer_network()
+    {
+        control.bit.wantsAck    = 0;
+        control.bit.isAck       = 0;
+        control.bit.isRequest   = 0;
+    };
+
+    void handle_receive(stack_message *msg) // TODO: handle wantsACk --> send ACK, isACk --> don't send package again
+    {
+        if (DEBUG) cout << "rNetwork ";
+        // handle header and payload
+        control.value = msg->payload[msg->position];
+        msg->size = ++msg->position;
+        // next layer
+        if (!is_top) p_upper_layer->handle_receive(msg);
+        // handle tail
+    };
+
+    void handle_transmit(stack_message *msg)
+    {
+        if (DEBUG) cout << "tNetwork ";
+        // handle header and payload
+        msg->payload[msg->position] = control.value;
+        msg->size = ++msg->position;
+        // next layer
+        if (!is_top) p_upper_layer->handle_transmit(msg);
+        // handle tail
+    };
+
+    void set_control(const uint8_t wantsAck = 0, const uint8_t isAck = 0, const uint8_t isRequest = 1)
+    {
+        control.bit.wantsAck    = wantsAck;
+        control.bit.isAck       = isAck;
+        control.bit.isRequest   = isRequest;
+    };
 
 };
 
--- src/layer_session.h
+++ src/layer_session.h
@@ -6,6 +6,7 @@
 #ifndef SENSOR_ACTOR_NETWORK_LAYER_SESSION_H
 #define SENSOR_ACTOR_NETWORK_LAYER_SESSION_H
 
+#include <stdint.h>
 #include "layer_interface.h"
 
 enum class Command : uint8_t    // underlying type
@@ -14,8 +15,8 @@ enum class Command : uint8_t    // underlying type
     registerNode,   // get new ID and integrate new node into network,
     unregister,     // node has to reannounce
     //subscribe,      // same es register? tbd. maybe specialize
-            shutdown,      // send to sleep for a specific time (�C + RF)
-    //suspend,         // send to sleep for a specific time (�C) --> needed?
+            shutdown,       // send to sleep for a specific time (uC + RF)
+    //suspend,        // send to sleep for a specific time (uC) --> needed?
 
     ping,           // to each node, possible to trigger a tree-search
     sendRawData,
@@ -23,21 +24,27 @@ enum class Command : uint8_t    // underlying type
     sendTrigger,
     sendActor,      // normally to Node
     // sendUndefined,
-            sendTime        // prefer as broadcast, so every node can sync
+    sendTime        // prefer as broadcast, so every node can sync
 };
 
-struct header_session
+typedef union
 {
-    Command   command;
-    uint8_t   pLength;
-    //uint8_t   payload[PAYLOAD_MAX]; // TODO: should this contain a pointer to the payload instead?
-};
+    uint8_t value[3];
+
+    struct
+    {
+        Command command;
+        uint8_t pLength;
+        //uint8_t   payload[PAYLOAD_MAX]; // TODO: should this contain a pointer to the payload instead?
+    } form;
+
+} header_session;
 
 class layer_session : public layer_interface
 {
     void handle_receive(stack_message *msg)
     {
-        if (DEBUG)  cout << "rSession ";
+        if (DEBUG) cout << "rSession ";
         // handle header and payload
         if (!is_top) p_upper_layer->handle_receive(msg);
         // handle tail
@@ -45,7 +52,7 @@ class layer_session : public layer_interface
 
     void handle_transmit(stack_message *msg)
     {
-        if (DEBUG)  cout << "tSession ";
+        if (DEBUG) cout << "tSession ";
         // handle header and payload
         msg->payload[msg->position++] = 1;
         msg->size++;
